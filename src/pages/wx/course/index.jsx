740b7cb4a5cf05088ef8175d3458109aa95293d9
张鲁燕 <zhangluyan@jsh.com>
2021-01-11 21:12:20 +0800

    update course

--- src/pages/wx/course/index.jsx
+++ src/pages/wx/course/index.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import "swiper/swiper-bundle.css";
+import Swiper from "swiper"
+import "swiper/dist/css/swiper.css";
 import "./course.css";
 import img1 from '../../../assets/imgs/1.png';
 import img2 from '../../../assets/imgs/2.png';
@@ -10,34 +9,63 @@ import img4 from '../../../assets/imgs/4.png';
 import img5 from '../../../assets/imgs/5.png';
 import img6 from '../../../assets/imgs/6.png';
 import img7 from '../../../assets/imgs/7.png';
-import img8 from '../../../assets/imgs/8.png';
-
-// install Swiper components
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 class Index extends Component {
+    componentDidMount(){
+        document.title = "淘宝省钱教程";
+        new Swiper ('.swiper-container', {
+            direction: 'vertical', // 垂直切换选项
+            loop: true, // 循环模式选项
+            height : window.innerHeight,
+            // 如果需要分页器
+            pagination: {
+                el: '.swiper-pagination',
+            }
+        })
+    }
     render() {
         return (
-            <Swiper
-                direction: "vertical"
-                spaceBetween={50}
-                slidesPerView={1}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
-            >
-                <SwiperSlide><img alt="1" src={img1}/></SwiperSlide>
-                <SwiperSlide><img alt="2" src={img2}/></SwiperSlide>
-                <SwiperSlide><img alt="3" src={img3}/></SwiperSlide>
-                <SwiperSlide><img alt="4" src={img4}/></SwiperSlide>
-                <SwiperSlide><img alt="5" src={img5}/></SwiperSlide>
-                <SwiperSlide><img alt="6" src={img6}/></SwiperSlide>
-                <SwiperSlide><img alt="7" src={img7}/></SwiperSlide>
-                <SwiperSlide><img alt="8" src={img8}/></SwiperSlide>
+            <div className="swiper-container">
+                <div className="swiper-wrapper">
+                    <div className="swiper-slide">
+                        <div className="course_content">
+                            <div className="swiper-title">淘宝天猫领券 1/7</div>
+                            <img alt='1' src={img1}/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div className="course_content">
+                            <div className="swiper-title">淘宝天猫领券 2/7</div>
+                            <img alt='2' src={img2}/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div className="course_content">
+                            <div className="swiper-title">淘宝天猫领券 3/7</div>
+                            <img alt='3' src={img3}/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div className="course_content">
+                            <div className="swiper-title">淘宝天猫领券 4/7</div>
+                            <img alt='4' src={img4}/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div className="course_content">
+                            <div className="swiper-title">淘宝天猫领券 5/7</div>
+                            <img alt='5' src={img5}/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div className="course_content">
+                            <div className="swiper-title">淘宝天猫领券 6/7</div>
+                            <img alt='6' src={img6}/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div className="course_content">
+                            <div className="swiper-title">淘宝天猫领券 7/7</div>
+                            <img alt='7' src={img7}/></div>
+                    </div>
+                </div>
+                <div className="swiper-pagination swiper-pagi"></div>
+            </div>
 
-            </Swiper>
         );
     }
 }
