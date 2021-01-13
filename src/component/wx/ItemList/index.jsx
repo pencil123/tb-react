19233d71348a0512869631c0de735c3ff14aa9b2
lyzhang <tianxiamall@163.com>
2021-01-13 17:57:08 +0800

    update recommend

--- src/component/wx/ItemList/index.jsx
+++ src/component/wx/ItemList/index.jsx
@@ -6,20 +6,34 @@ import './itemlist.css';
 
 class Index extends Component {
   render() {
+    const now = 60;
     return (
         <div className="itemlist">
-          <bs.Row>
+          <bs.Row className="itemCon">
             <bs.Col>
               <bs.Image src={testimg} rounded className="itemImg" />
             </bs.Col>
             <bs.Col>
-              <bs.Card style={{ width: '18rem' }}>
-                <bs.Card.Header>环境是非常重要的资源，需要大家爱惜对待</bs.Card.Header>
-                <bs.Card.Body>
-                  <bs.Card.Title>这里是Title,字体会大一些</bs.Card.Title>
-                  <bs.Card.Text>这里是文本内容，字体会小一些。</bs.Card.Text>
-                </bs.Card.Body>
-              </bs.Card>
+              <div>
+                <div className="itemTitle">环境是非常重要的资源，需要大家爱惜对待</div>
+                <div className="itemLabel"><span>巍峨高山</span><span>静水深流</span></div>
+                <div className="itemProg"><bs.ProgressBar animated now={now} label={`剩余  ${now}%`} /></div>
+                <div className="item_price"><span className="item_curprice">￥99 / </span><span className="item_oldprice">￥199</span><span className="donow">| 立即行动</span></div>
+              </div>
+            </bs.Col>
+          </bs.Row>
+
+          <bs.Row className="itemCon">
+            <bs.Col>
+              <bs.Image src={testimg} rounded className="itemImg" />
+            </bs.Col>
+            <bs.Col>
+              <div>
+                <div className="itemTitle">环境是非常重要的资源，需要大家爱惜对待</div>
+                <div className="itemLabel"><span>巍峨高山</span><span>静水深流</span></div>
+                <div className="itemProg"><bs.ProgressBar animated now={now} label={`剩余  ${now}%`} /></div>
+                <div className="item_price"><span className="item_curprice">￥99 / </span><span className="item_oldprice">￥199</span><span className="donow">| 立即行动</span></div>
+              </div>
             </bs.Col>
           </bs.Row>
         </div>
