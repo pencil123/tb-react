5768a6740405df9aaf97b2733907b8d047b2585d
张鲁燕 <zhangluyan@jsh.com>
2021-01-13 23:27:37 +0800

    update recommend

--- src/pages/wx/recommend/index.jsx
+++ src/pages/wx/recommend/index.jsx
@@ -3,17 +3,22 @@ import * as bs from "react-bootstrap";
 
 import ItemList from '../../../component/wx/ItemList';
 import './recommend.css';
+import youxuanimg from "../../../assets/imgs/youxuan.png";
 
 class Index extends Component {
-  render() {
-    return (
-        <bs.Container fluid className="recommCon">
-          <bs.Row className="recommTitle">每日优选</bs.Row>
-          <ItemList/>
-          <bs.Row>Buttom</bs.Row>
-        </bs.Container>
-    );
-  }
+    render() {
+        return (
+            <bs.Container fluid className="recommCon">
+                <bs.Row className="recommTitle">
+                    <bs.Image src={youxuanimg} rounded/>
+                </bs.Row>
+                <ItemList/>
+                <bs.Row className="itemfooter">
+                    <span className="footercontent">我也是有底线地</span>
+                </bs.Row>
+            </bs.Container>
+        );
+    }
 }
 
 export default Index;
