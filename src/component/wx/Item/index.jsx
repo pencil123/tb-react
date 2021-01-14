f96b533d7d9a5069420d55b5cda7ef1c2a5d2804
lyzhang <tianxiamall@163.com>
2021-01-14 09:53:41 +0800

    update recommend

---
+++ src/component/wx/Item/index.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import * as bs from "react-bootstrap";
+
+import testimg from '../../../assets/temp/test.jpg';
+import './item.css';
+
+class Index extends Component {
+  render() {
+    const now = 60;
+    return (
+        <bs.Row className="itemCon">
+          <bs.Col>
+            <bs.Image src={testimg} rounded className="itemImg"/>
+          </bs.Col>
+          <bs.Col>
+            <div>
+              <div className="itemTitle">环境是非常重要的资源，需要大家爱惜对待</div>
+              <div className="itemLabel"><span>巍峨高山</span><span>静水深流</span>
+              </div>
+              <div className="itemProg">
+                <bs.ProgressBar animated now={now} label={`剩余  ${now}%`}/>
+              </div>
+              <div className="item_price"><span
+                  className="item_curprice">￥99 / </span><span
+                  className="item_oldprice">￥199</span><span className="donow">| 立即行动</span>
+              </div>
+            </div>
+          </bs.Col>
+        </bs.Row>
+    );
+  }
+}
+
+export default Index;
--- src/pages/wx/recommend/index.jsx
+++ src/pages/wx/recommend/index.jsx
@@ -1,24 +1,26 @@
 import React, {Component} from 'react';
 import * as bs from "react-bootstrap";
 
-import ItemList from '../../../component/wx/ItemList';
+import Item from '../../../component/wx/Item';
 import './recommend.css';
 import youxuanimg from "../../../assets/imgs/youxuan.png";
 
 class Index extends Component {
-    render() {
-        return (
-            <bs.Container fluid className="recommCon">
-                <bs.Row className="recommTitle">
-                    <bs.Image src={youxuanimg} rounded/>
-                </bs.Row>
-                <ItemList/>
-                <bs.Row className="itemfooter">
-                    <span className="footercontent">我也是有底线地</span>
-                </bs.Row>
-            </bs.Container>
-        );
-    }
+  render() {
+    return (
+        <bs.Container fluid className="recommCon">
+          <bs.Row className="recommTitle">
+            <bs.Image src={youxuanimg} rounded/>
+          </bs.Row>
+          <div className="itemlist">
+            <Item /><Item /><Item /><Item /><Item /><Item /><Item /><Item /><Item /><Item />
+          </div>
+          <bs.Row className="itemfooter">
+            <span className="footercontent">我也是有底线地</span>
+          </bs.Row>
+        </bs.Container>
+    );
+  }
 }
 
 export default Index;
