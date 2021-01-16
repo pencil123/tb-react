b2ae09715c8d43652b692af33d9c1ae268e5139d
张鲁燕 <zhangluyan@jsh.com>
2021-01-16 10:41:08 +0800

    浮点数相减

--- src/component/wx/Item/index.jsx
+++ src/component/wx/Item/index.jsx
@@ -6,8 +6,8 @@ import './item.css';
 class Index extends Component {
   render() {
     const {item_id, title, pict_url, zk_final_price, coupon_total_count, coupon_remain_count, coupon_info, coupon_start_fee, coupon_amount} = this.props.item;
-    const now = Math.round(coupon_remain_count / coupon_total_count * 10000)
-        / 100.00;
+    const now = Math.round(coupon_remain_count / coupon_total_count * 10000)  / 100.00;
+    const final_price = Math.round((zk_final_price - coupon_amount) * 10000) / 100.00;
     return (
         <bs.Row className="itemCon">
           <bs.Col>
@@ -28,8 +28,7 @@ class Index extends Component {
               <div className="item_price">
                 <a href={"/wx/coupon/#" + item_id}>
                 <span
-                    className="item_curprice">￥{zk_final_price
-                - coupon_amount} / </span><span
+                    className="item_curprice">￥{final_price} / </span><span
                     className="item_oldprice">￥{zk_final_price}</span><span
                     className="donow">| 购买</span>
                 </a>
